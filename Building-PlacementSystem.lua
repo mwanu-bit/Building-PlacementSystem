28f4935c14df1a76ab9b3b5f93a251e59c63228e
mwanu-bit <nexblake6@gmail.com>
2026-01-21 22:11:08 +0300

    Update Building-PlacementSystem.lua

--- Building-PlacementSystem.lua
+++ Building-PlacementSystem.lua
@@ -1,6 +1,7 @@
 --Desc:Script handles a building system similar to the one in games such as Work At a Pizza Place
 --The system handles teh following features: Core Building Logic such as Placement of Build,Rotation of Builds to be Placed and Destruction of Placed Builds
 --The system is a combination of what would have been two scripts a handler and class scripts
+--Game link works now
 ------[[MODULE]]-----
 local BuildingHandler = {}
 BuildingHandler.__index = BuildingHandler
